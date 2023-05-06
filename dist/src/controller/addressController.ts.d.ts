9476202a44c05c509ca111221885c2645db41734
anhkhonglanman <quyen283hn@gmail.com>
2023-05-06 16:33:16 +0700

    try

--- dist/src/controller/addressController.ts.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-declare class UserController {
-    signup: (req: Request, res: Response) => Promise<void>;
-    showUser: (req: Request, res: Response) => Promise<any>;
-    editUser: (req: Request, res: Response) => Promise<void>;
-    login: (req: Request, res: Response) => Promise<void>;
-}
-declare const _default: UserController;
-export default _default;
